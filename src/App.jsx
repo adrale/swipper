ea45142d6bfc8c3033453adddec6c64cbd1e177f
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-05-13 11:55:28 +0200

    fetch into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import './App.css'
 import SwipperCard from './components/SwipperCard'
 
 function App() {
+  const [lover, setLover] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchLover();
+  }, []);
+
+  const fetchLover = () => {
+    fetch("https://randomuser.me/api?nat=en")
+      .then((res) => res.json())
+      .then((data) => {
+        setLover(data.results[0]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération du Lover :", error);
+        setLoading(false);
+      });
+  };
 
   return (
     <>
-      <SwipperCard/>
+      <SwipperCard lover={lover} loading={loading}/>
     </>
   )
 }
