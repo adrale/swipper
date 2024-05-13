203f468ae9c10c8d901b3261db32403207a1a323
Jade Jolivet <jade.jolivet@gmail.com>
2024-05-13 11:33:37 +0200

    SwipperCard + SwipperLogic

---
+++ src/components/SwipperLogic.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Card from '../components/SwipperCard';
+
+const SwipperLogic = ({ profil }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleLike = () => {
+    setCurrentIndex(prevIndex => (prevIndex === profil.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handleDislike = () => {
+    setCurrentIndex(prevIndex => (prevIndex === profil.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <section className="swipe-logic">
+      <section className="card-wrapper">
+        <SwipperCard profils={profil[currentIndex]} onLike={handleLike} onDislike={handleDislike} />
+      </section>
+    </section>
+  );
+};
+
+export default SwipperLogic;
